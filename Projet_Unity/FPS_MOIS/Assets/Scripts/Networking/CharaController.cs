088c3716b5b8ecca0eda451fdd8c955e5cb89929
Sébastien Fouque <emailsebas.fouque@gmail.com>
2015-03-24 15:30:05 +0100

    Premier Test Networking

---
+++ Projet_Unity/FPS_MOIS/Assets/Scripts/Networking/CharaController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharaController : MonoBehaviour {
+
+	NetworkView nView;
+
+	public float speed = 5f;
+	public float speedRotate = 1f;
+
+	[RPC]
+	public void MoveForward(){
+		transform.Translate(Vector3.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"));
+	}
+
+	[RPC]
+	public void MoveRotate(){
+		transform.Rotate(0,speedRotate * Input.GetAxis("Horizontal"), 0);
+	}
+
+
+	void Start () {
+		nView = GetComponent<NetworkView> ();
+	}
+	
+
+	void Update () {
+		if(Input.GetAxis("Vertical") != 0){
+			nView.RPC("MoveForward", RPCMode.All);
+		}
+		if(Input.GetAxis("Horizontal") != 0){
+			nView.RPC("MoveRotate", RPCMode.All);
+		}
+	}
+}
