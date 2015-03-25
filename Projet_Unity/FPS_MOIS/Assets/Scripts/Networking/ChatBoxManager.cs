119d2ce6d1db55afbfbe9e8ee4ddeeab4d5e4b7d
Sébastien Fouque <sebas.fouque@gmail.com>
2015-03-25 16:06:59 +0100

    ChatBox locale fonctionnelle

---
+++ Projet_Unity/FPS_MOIS/Assets/Scripts/Networking/ChatBoxManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ChatBoxManager : MonoBehaviour {
+
+	public GameObject chatBox;
+	Text chatBoxText;
+	RectTransform chatBoxRectTransform;
+	public Scrollbar chatBoxScrollBar;
+	public InputField chatBoxInputField;
+
+	bool isChatActive = false;
+
+
+	static ChatBoxManager mInst;
+	static public ChatBoxManager instance { get { return mInst; } }		
+
+	void Awake () {
+		if(mInst == null) mInst = this;
+		DontDestroyOnLoad(this); 		
+	}
+
+	// Send message chatBox Custom
+	public IEnumerator SendMessage(string source, string message){
+		
+		Vector2 chatboxSize = chatBoxRectTransform.sizeDelta;
+		
+		chatBoxText.text += "<" + source + "> : " + message + "\n\r";
+		chatboxSize.y += (chatBoxText.lineSpacing*2 + chatBoxText.fontSize);
+		chatBoxRectTransform.sizeDelta = chatboxSize;
+		yield return new WaitForEndOfFrame();
+		chatBoxScrollBar.value = 0;
+	}
+
+	void Start () {
+		chatBoxRectTransform = chatBox.GetComponent<RectTransform>();
+		chatBoxText= chatBox.GetComponent<Text>();
+		StartCoroutine(SendMessage("GAME", "Ready"));
+	}
+
+	void Update () {
+
+
+
+			ColorBlock colorBlock = chatBoxInputField.colors;
+			
+			if((Input.GetKeyDown(KeyCode.Return) && !isChatActive)  || chatBoxInputField.isFocused == true){
+				chatBoxInputField.ActivateInputField();
+				colorBlock.colorMultiplier = 2;
+				chatBoxInputField.colors = colorBlock;
+				isChatActive = true;
+			}
+			if((Input.GetKeyDown(KeyCode.Return) && isChatActive == true)  || chatBoxInputField.isFocused == false){
+				if(chatBoxInputField.text != ""){
+					StartCoroutine(SendMessage("PLAYER", chatBoxInputField.text));
+				}
+				chatBoxInputField.text = "";
+				chatBoxInputField.DeactivateInputField();
+				colorBlock.colorMultiplier = 1;
+				chatBoxInputField.colors = colorBlock;
+				isChatActive = false;
+			}
+
+	}
+}
--- Projet_Unity/FPS_MOIS/Assets/Scripts/Networking/Networking.cs
+++ Projet_Unity/FPS_MOIS/Assets/Scripts/Networking/Networking.cs
@@ -1,23 +1,96 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Networking : MonoBehaviour {
 
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "RoomName";
+	private HostData[] hostList;
 
-	public void CreateServer(){
-		Network.InitializeServer (2, 25000, false);
+
+
+	// ========================= Fonctions GUI =========================
+
+	// Demarrage du serveur GUI
+	public void GUIStartServer(){
+		if (!Network.isClient && !Network.isServer)
+		{
+			StartServer();
+		}else{
+			StartCoroutine(ChatBoxManager.instance.SendMessage("GAME", "Server already initialized !"));
+		}
 	}
 
-	public void JoinServer(){
-		Network.Connect("10.51.0.218", 25000);
+	// Rejoindre le serveur GUI
+	public void GUIJoinServer(){
+		if (!Network.isClient && !Network.isServer)
+		{
+			JoinServer(hostList[0]);
+		}
 	}
 
-	void Start () {
-		
+	// Refresh la liste des hotes GUI
+	public void GUIRefreshHostList(){
+		if (!Network.isClient && !Network.isServer)
+		{
+			RefreshHostList();
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// ========================= Fonctions du serveur =========================
+
+	void Start(){
+
+	}
 	
+	// Demarrage du serveur
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+		MasterServer.ipAddress = "127.0.0.1";
+	}
+
+	// Appelée automatiquement quand le serveur est initialisé
+	void OnServerInitialized()
+	{
+		StartCoroutine(ChatBoxManager.instance.SendMessage("GAME", "Server initialized !"));
+	}
+
+	// Refresh la liste des hotes
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+
+	// On MasterServer event
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	// Rejoindre le serveur
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+
+	// RAppelée automatiquement quand connecté au serveur
+	void OnConnectedToServer()
+	{
+		StartCoroutine(ChatBoxManager.instance.SendMessage("GAME", "Server joined !"));
+	}
+
+	void Update(){
+		if (hostList != null)
+		{
+			for (int i = 0; i < hostList.Length; i++)
+			{
+				Debug.Log(hostList[i].gameName);
+					//JoinServer(hostList[i]);
+			}
+		}
 	}
 }
