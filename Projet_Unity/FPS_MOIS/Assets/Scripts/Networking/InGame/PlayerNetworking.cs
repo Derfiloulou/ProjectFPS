a44dcd5e7e0e9261e7bd9c088e14c8d34b41ad5b
Sébastien Fouque <sebas.fouque@gmail.com>
2015-04-15 10:13:14 +0200

    Prédiction

--- Projet_Unity/FPS_MOIS/Assets/Scripts/Networking/InGame/PlayerNetworking.cs
+++ Projet_Unity/FPS_MOIS/Assets/Scripts/Networking/InGame/PlayerNetworking.cs
@@ -37,21 +37,28 @@ public class PlayerNetworking : MonoBehaviour {
 	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
 	{
 		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
+
+
 		if (stream.isWriting)
 		{
 			syncPosition = playerRigidbody.position;
 			stream.Serialize(ref syncPosition);
+
+			syncVelocity = playerRigidbody.velocity;
+			stream.Serialize(ref syncVelocity);
 		}
 		else
 		{
 			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncVelocity);
 			
 			syncTime = 0f;
 			syncDelay = Time.time - lastSynchronizationTime;
 			lastSynchronizationTime = Time.time;
 			
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
 			syncStartPosition = playerRigidbody.position;
-			syncEndPosition = syncPosition;
 		}
 	}
 
